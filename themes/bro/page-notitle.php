6982d2b577584c4b878e2f7170122261b7461e63
ryant <ryan.thorp@my.jcu.edu.au>
2016-05-16 11:37:28 +1000

    header , no title page
    default header created along with separate home page template without
    title being dispalyed

---
+++ themes/bro/page-notitle.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Full Width Page No Title
+*/
+?>
+<?php
+/**
+ Template for displaying pages with no title
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package BRO
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page-notitle' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
